eace94f2114b68b3ffea965f1ad881cdbe20b7d4
stefan <stefan.louie@gmail.com>
2022-02-11 16:44:35 +1000

    Added in page for employees and got GET functionality working in a table.

--- frontend/frontend_root/pages/urls.py
+++ frontend/frontend_root/pages/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('employees', views.get_employees, name='employees')
 ]
--- frontend/frontend_root/pages/views.py
+++ frontend/frontend_root/pages/views.py
@@ -1,4 +1,6 @@
+from asyncio.windows_events import NULL
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 """
@@ -6,3 +8,14 @@ Render the home page of the website.
 """
 def index(request):
     return render(request, 'base.html')
+
+
+"""
+Render the employees page, displaying the list of employees.
+"""
+def get_employees(request):
+    employees = requests.get('http://localhost:3000/employees')
+    
+    context = {"employees": employees.json()}
+    
+    return render(request, 'pages/employees.html', context)
